11f5ff40a2b3bf1990bfeb59974aee307fc1117c
Hanif Olayiwola <110940109+TheNonFungibleHuman@users.noreply.github.com>
2026-05-03 15:55:44 +0400

    feat: Add referrerPolicy to YouTube embed
    Improves privacy by preventing the YouTube embed from sending referrer information to the video host.

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -652,11 +652,12 @@ export default function Course() {
                       key={(currentPageData as any).youtubeId}
                       width="100%" 
                       height="100%" 
-                      src={`https://www.youtube-nocookie.com/embed/${(currentPageData as any).youtubeId}?autoplay=0&origin=${window.location.origin}`} 
+                      src={`https://www.youtube-nocookie.com/embed/${(currentPageData as any).youtubeId}?autoplay=0`} 
                       title={currentPageData.title}
                       frameBorder="0" 
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                       allowFullScreen
+                      referrerPolicy="no-referrer"
                       onLoad={() => setVideoLoading(false)}
                       className="relative z-0"
                     ></iframe>
